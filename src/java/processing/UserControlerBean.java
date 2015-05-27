67cdac324bf4faa316247631ad5e75d6971f77fd
Angele <giraudae@hotmail.fr>
2015-05-27 11:40:07 +0200

    UserControlerBean check and add new user

--- src/java/processing/UserControlerBean.java
+++ src/java/processing/UserControlerBean.java
@@ -5,7 +5,6 @@
  */
 package processing;
 
-
 import java.util.Map;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
@@ -14,8 +13,9 @@ import javax.faces.context.FacesContext;
 
 import model.UserSubmissionModelBean;
 import dao.instance.UserDao;
+import dao.fabric.DaoFabric;
 import model.LoginBean;
-
+import model.UserModelBean;
 
 /**
  *
@@ -25,36 +25,72 @@ import model.LoginBean;
 @ApplicationScoped // Utilisation de application scope afin d'offrir un point d'entrée unique à l'ensemble des clients
 
 public class UserControlerBean {
+
     private UserDao userDao;
 
     public UserControlerBean() {
         this.userDao = DaoFabric.getInstance().createUserDao();
     }
 
-    public String checkUser(LoginBean loginBean){
-        UserModelBean user = this.userDao.checkUser(loginBean.getLogin(), loginBean.getPwd());
-        if( user!=null){
-            //récupère l'espace de mémoire de JSF
-            ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
-            Map<String, Object> sessionMap = externalContext.getSessionMap();
-            //place l'utilisateur dans l'espace de mémoire de JSF
-            sessionMap.put("loggedUser", user);
-            //redirect the current page
-            return "userdisplay.xhtml";
-        }else{
-            //redirect the current page
-            return "userLogin.xhtml";
-        }
-    }    
+    public String checkUser(LoginBean loginBean) {
+        UserModelBean user = userDao.GetUser(loginBean.getLogin());
 
-    public void checkAndAddUser(UserSubmissionModelBean userSubmitted){
-        //Vérifier les propriétés de l'utilisateur
-        if (userSubmitted.pwd1==userSubmitted.pwd2) {
-            
+        if (user != null) {
+            if (user.getPwd().equals(loginBean.getPwd())) {
+                //récupère l'espace de mémoire de JSF
+                ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+                Map<String, Object> sessionMap = externalContext.getSessionMap();
+                //place l'utilisateur dans l'espace de mémoire de JSF
+                sessionMap.put("loggedUser", user);
+                //redirect the current page
+                return "userdisplay.xhtml"; // TODO à modifier par bon nom
+            }
+            else {
+                // Message : Mot de passe mauvais
+                // TODO
+                return "userLogin.xhtml";   // TODO à modifier par bon nom
+            }
+        } else {
+            // Message : login inexistant
+            // TODO
+            //redirect the current page
+            return "userLogin.xhtml";   // TODO à modifier par bon nom
         }
-        //TODO
-        //UserSubmissionModelBean
-        //ajout de l'utilisateur à la base de données
-        this.userDao.addUser(userSubmitted);
+    }
+
+    public String checkAndAddUser(UserSubmissionModelBean userSubmitted) {
+        //Vérifier les propriétés de l'utilisateur
+        if (userSubmitted.getPwd().equals(userSubmitted.getPwd2())) {
+            int i = 0;
+
+            for (String login : userDao.getUsersLogin()) {
+                if (userSubmitted.getLogin().equals(login)) {
+                    i = 1;
+                }
+            }
+            if (i == 1) {
+                // Message : login déjà pris
+                // TODO
+                //redirect the count creation page
+                return "userCountCreation.xhtml"; // TODO
+            } else {
+                //UserSubmissionModelBean
+                //ajout de l'utilisateur à la base de données
+                this.userDao.addUser(userSubmitted);
+                //pop-up compte créé
+                return "userCountCreated.xhtml"; // TODO
+            }
+
+            /*while ((!userSubmitted.getLogin().equals(userDao.getUsersLogin().get(i))) || (i == userDao.getUsersLogin().size)) {
+             i++;
+             }
+             if (userSubmitted.getLogin() != userDao.getUsersLogin().get(i)) {            
+             }*/
+        } else {
+            // Message : 2 mdp donnés différents
+            // TODO
+            //redirect the count creation page
+            return "userCountCreation.xhtml";
+        }
     }
 }
